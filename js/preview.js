5a2608112924fea508cf847edf8aa4b3f28e6a94
Edy Segura <ednardo@inatel.br>
2017-11-20 23:21:40 -0200

    Load image as base64

--- js/preview.js
+++ js/preview.js
@@ -1,6 +1,15 @@
 
 const upload = document.getElementById('upload')
+const preview = document.getElementById('preview')
+
+function showPreview(file) {
+    const fileReader = new FileReader()
+    fileReader.onloadend = () => {
+        preview.src = fileReader.result
+    }
+    fileReader.readAsDataURL(file)
+}
 
 upload.addEventListener('change', e => {
-    console.log(e.target.files)
+    showPreview(e.target.files[0])
 })
