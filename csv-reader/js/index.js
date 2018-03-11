49ff96040f34ba6f510962f035f72581f18e5ab7
Edy Segura <ednardo@inatel.br>
2018-03-11 12:43:05 -0300

    Renaming methods

--- csv-reader/js/index.js
+++ csv-reader/js/index.js
@@ -6,22 +6,25 @@ function showData(data) {
 }
 
 function processCSV(csvData) {
-  const lineDelimiter = '\n', colunmDelimiter = ','
+  const lineDelimiter = '\n'
+  const colunmDelimiter = ','
+
   const output =
     csvData
       .split(lineDelimiter)
       .map(line => line.split(colunmDelimiter))
+
   return output
 }
 
-function readCSVFile(csvFile) {
+function loadCSVFile(csvFile) {
   const reader = new FileReader()
   reader.onloadstart = () => output.textContent = 'Loading...'
   reader.onloadend = ( event ) => showData(processCSV(event.target.result))
-  reader.onerror = () => output.textContent = 'Could not read the file'
+  reader.onerror = () => output.textContent = 'This file could not be read'
   reader.readAsText(csvFile)
 }
 
 input.addEventListener('change', (event) => {
-  readCSVFile(event.currentTarget.files[0])
+  loadCSVFile(event.currentTarget.files[0])
 })
