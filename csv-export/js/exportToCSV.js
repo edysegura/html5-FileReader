dc5f7b3213dcdc7570859a8ac148bd6c8a2470a4
Edy Segura <ednardo@inatel.br>
2018-04-05 23:04:21 -0300

    Refactoring

--- csv-export/js/exportToCSV.js
+++ csv-export/js/exportToCSV.js
@@ -1,7 +1,14 @@
-export default function exportToCSV(filename, data) {
+'use strict'
+
+function arrayToCSV(data) {
   let csvText = ''
   data.forEach(row => {
     csvText += row.join(',') + '\n'
-  });
+  })
+  return csvText
+}
+
+export default function exportToCSV(filename, data) {
+  let csvText = arrayToCSV(data)
   console.log(csvText)
 }
