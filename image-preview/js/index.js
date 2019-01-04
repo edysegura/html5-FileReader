f260efae22a773d73709d571a16922263432f10c
Edy Segura <edysegura@gmail.com>
2019-01-04 14:15:08 -0200

    Refactoring index.js to support multiple files

--- image-preview/js/index.js
+++ image-preview/js/index.js
@@ -1,15 +1,17 @@
-
 const upload = document.getElementById('upload')
 const preview = document.getElementById('preview')
 
 function showPreview(file) {
-    const fileReader = new FileReader()
-    fileReader.onloadend = () => {
-        preview.src = fileReader.result
-    }
-    fileReader.readAsDataURL(file)
+  const fileReader = new FileReader()
+  fileReader.onloadend = () => {
+    preview.src = fileReader.result
+  }
+  fileReader.readAsDataURL(file)
 }
 
 upload.addEventListener('change', event => {
-    showPreview(event.target.files[0])
+  const files = event.target.files
+  for(const file of files) {
+    showPreview(file)
+  }
 })
