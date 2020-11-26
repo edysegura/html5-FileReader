cde953ee9ad13b6b18e1515564f0243396b36be4
Edy Segura <edysegura@gmail.com>
2020-11-26 20:25:37 -0300

    Add Local Download approach to csv-export Fixes #21

--- csv-export/sw.js
+++ csv-export/sw.js
@@ -1,12 +1,25 @@
+importScripts('./js/data-sw.js')
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  self.clients.claim()
+})
+
+function loadContent() {
+  const filename = 'my-file.txt'
+  const downloadResponse = new Response(arrayToCSV(data))
+  downloadResponse.headers.append(
+    'Content-Disposition',
+    `attachment; filename="${filename}"`
+  )
+  return downloadResponse
+}
+
 self.addEventListener('fetch', (event) => {
   if (event.request.url.includes('download-file')) {
-    const filename = 'my-filename.csv'
-    const data = 'testing...'
-    const response = new Response(data)
-    response.headers.append(
-      'Content-Disposition',
-      `attachment; filename="${filename}"`
-    )
-    event.respondWith(response)
+    event.respondWith(loadContent())
   }
 })
