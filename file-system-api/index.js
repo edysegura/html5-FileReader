d2913c640462afd0422ebd59df9b3c6d011f0486
Edy Segura <edysegura@gmail.com>
2021-01-12 22:55:58 -0300

    Add HTML output

--- file-system-api/index.js
+++ file-system-api/index.js
@@ -1,7 +1,15 @@
 const button = document.querySelector('button')
+const output = document.querySelector('output')
 
 button.addEventListener('click', async () => {
   const [ handle ] = await window.showOpenFilePicker()
   const file = await handle.getFile()
+
   console.log(file)
+
+  output.innerHTML = `
+    <ul>
+      <li>File name: ${file.name}</li>
+    </ul>
+  `
 })
