fd1e578b75f0afbae01fbc9611de00acbcaf1c21
Edy Segura <edysegura@gmail.com>
2021-01-13 17:32:09 -0300

    Enhance File System example

--- file-system-api/index.js
+++ file-system-api/index.js
@@ -1,6 +1,10 @@
 const button = document.querySelector('button')
 const output = document.querySelector('output')
 
+function convertToKB(size) {
+  return size / 1024
+}
+
 button.addEventListener('click', async () => {
   const [ handle ] = await window.showOpenFilePicker()
   const file = await handle.getFile()
@@ -10,6 +14,8 @@ button.addEventListener('click', async () => {
   output.innerHTML = `
     <ul>
       <li>File name: ${file.name}</li>
+      <li>File type: ${file.type || "Unknow"}</li>
+      <li>File size: ${convertToKB(file.size).toFixed(2)} KB</li>
     </ul>
   `
 })
