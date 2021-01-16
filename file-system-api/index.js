1699ee650eb6cde9960ab6e1f96b4e3600bf9ae2
Edy Segura <edysegura@gmail.com>
2021-01-15 22:46:14 -0300

    Change innerHTML to textContent

--- file-system-api/index.js
+++ file-system-api/index.js
@@ -22,7 +22,7 @@ btnGetFile.addEventListener('click', async () => {
 })
 
 btnSaveFile.addEventListener('click', async () => {
-  const textFile = new File([ output.innerHTML ], 'hello.txt', {
+  const textFile = new File([ output.textContent ], 'hello.txt', {
     type: 'text/plain',
   })
   const handle = await window.showSaveFilePicker()
