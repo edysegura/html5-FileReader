43bb9ffa54219fbf8c589dbc96e3680ed663076a
Edy Segura <edysegura@gmail.com>
2021-01-15 22:44:33 -0300

    Use file properties as file writable content

--- file-system-api/index.js
+++ file-system-api/index.js
@@ -22,7 +22,7 @@ btnGetFile.addEventListener('click', async () => {
 })
 
 btnSaveFile.addEventListener('click', async () => {
-  const textFile = new File(['Hello File System API'], 'hello.txt', {
+  const textFile = new File([ output.innerHTML ], 'hello.txt', {
     type: 'text/plain',
   })
   const handle = await window.showSaveFilePicker()
