4810cd0590a7953023cefb4b60eb73f5e4cf3cba
Edy Segura <edysegura@gmail.com>
2023-03-15 22:04:56 -0300

    feat: add csv papa parse example

---
+++ csv-reader-papaparse/js/index.js
@@ -0,0 +1,61 @@
+import Papa from 'https://cdn.jsdelivr.net/npm/papaparse@5.4.0/+esm'
+
+const uploadContainer = document.getElementById('upload-container')
+const input = document.getElementById('upload')
+const output = document.querySelector('output')
+
+function showData(data) {
+  const header = data.shift()
+
+  data.splice(50)
+
+  const lines = data.map(line => `
+    <tr>
+      <td>${ line.join('</td><td>') }</td>
+    </tr>
+  `)
+
+  const tableHtml = `
+    <table>
+      <tr>
+        <th>${ header.join('</th><th>') }</th>
+      </tr>
+      ${ lines.join('') }
+    </table>
+  `
+
+  output.innerHTML = tableHtml
+}
+
+async function processCSV(file) {
+  Papa.parse(file, {
+    header: true,
+    worker: true,
+    complete: function(results) {
+      console.log("Finished:", results);
+      output.textContent = JSON.stringify(results.data, null, 2)
+    }
+  });
+}
+
+input.addEventListener('change', event => {
+  const [ firstSelectedFile ] = event.currentTarget.files
+  processCSV(firstSelectedFile)
+})
+
+uploadContainer.addEventListener('dragover', event => {
+  uploadContainer.classList.add('dragover')
+  event.preventDefault()
+})
+
+uploadContainer.addEventListener('dragleave', event => {
+  uploadContainer.classList.remove('dragover')
+  event.preventDefault()
+})
+
+uploadContainer.addEventListener('drop', event => {
+  const [ firstDroppedFile ] = event.currentTarget.files
+  processCSV(firstDroppedFile)
+  uploadContainer.classList.remove('dragover')
+  event.preventDefault()
+})
