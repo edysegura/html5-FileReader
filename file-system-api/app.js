b3f20bc7053423ab75dee3955b11ab63811c59bd
Edy Segura <edysegura@gmail.com>
2026-01-19 17:09:01 -0300

    feat: implement a layout for file-system-api example

--- file-system-api/app.js
+++ file-system-api/app.js
@@ -1,32 +1,41 @@
 const btnGetFile = document.getElementById('btnGetFile')
 const btnSaveFile = document.getElementById('btnSaveFile')
-const output = document.querySelector('output')
+const output = document.getElementById('output')
 
 function convertToKB(size) {
   return size / 1024
 }
 
 btnGetFile.addEventListener('click', async () => {
-  const [handle] = await window.showOpenFilePicker()
-  const file = await handle.getFile()
+  try {
+    const [handle] = await window.showOpenFilePicker()
+    const file = await handle.getFile()
 
-  console.log(file)
+    console.log(file)
 
-  output.innerHTML = `
-    <ul>
-      <li>File name: ${file.name}</li>
-      <li>File type: ${file.type || 'Unknown'}</li>
-      <li>File size: ${convertToKB(file.size).toFixed(2)} KB</li>
-    </ul>
-  `
+    output.innerHTML = `
+      <ul>
+        <li>File name: ${file.name}</li>
+        <li>File type: ${file.type || 'Unknown'}</li>
+        <li>File size: ${convertToKB(file.size).toFixed(2)} KB</li>
+      </ul>
+    `
+  } catch (err) {
+    output.textContent = 'No file selected.'
+    console.warn(err)
+  }
 })
 
 btnSaveFile.addEventListener('click', async () => {
-  const textFile = new File([output.textContent], 'document.txt', {
-    type: 'text/plain',
-  })
-  const handle = await window.showSaveFilePicker()
-  const writable = await handle.createWritable()
-  await writable.write(textFile)
-  await writable.close()
+  try {
+    const textFile = new File([output.textContent], 'document.txt', {
+      type: 'text/plain',
+    })
+    const handle = await window.showSaveFilePicker()
+    const writable = await handle.createWritable()
+    await writable.write(textFile)
+    await writable.close()
+  } catch (err) {
+    console.warn(err)
+  }
 })
